aa8e48fa4d2c23a9135048111e31e3d405b2c207
kaiofprates <kaioprates@hotmail.com>
2020-04-26 23:11:13 -0300

    add location and tags

--- src/controlers/index.js
+++ src/controlers/index.js
@@ -74,7 +74,7 @@ router.post('/commerce/register', async (req, res) => {
         const { user, commerceName, description, phone, tags } = req.body
         const auth = await profileModel.find({ user });
         if (auth) {
-            const data = await commerceModel.create({ user, commerceName, description, phone, tags });
+            const data = await commerceModel.create({ user, commerceName, description, phone, tags, location });
             const commerce = await profileModel.findById(user);
             console.log(commerce);
             return res.status(200).json(data);
@@ -163,10 +163,11 @@ router.post('/commerce/update', async (req, res) => {
         description,
         commerceName,
         tags,
-        phone
+        phone,
+        location
     } = req.body
 
-    const result = await commerceModel.update({ user }, { description, commerceName, tags, phone });
+    const result = await commerceModel.update({ user }, { description, commerceName, tags, phone, location });
     if (result) {
         res.json(result)
     } else {
